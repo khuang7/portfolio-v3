734affe3a6ba51ec9f1d84bceeb149b2ec78ab5c
Kevin Huang <khuang771@gmail.com>
2023-10-22 19:34:15 +1100

    first push to main

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        neoblue: "#c9dcd8",
+        neopurple: "#BC95D4",
+        neoyellow: "#eab308",
+      },
+    },
+    extend: {
+      animation: {
+        marquee: "marquee 12s linear infinite",
+        marquee2: "marquee2 12s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
